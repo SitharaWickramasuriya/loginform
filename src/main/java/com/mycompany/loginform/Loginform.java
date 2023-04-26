bd72d0365122f0ce0c4d590d473ad3dd12c0a249
HP <HP@DESKTOP-QBISOVH>
2023-04-26 22:27:56 +0530

    Github Java Project

---
+++ src/main/java/com/mycompany/loginform/Loginform.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginform;
+
+/**
+ *
+ * @author HP
+ */
+public class Loginform {
+
+    public static void main(String[] args) {
+        Registration rg = new Registration();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(Registration.EXIT_ON_CLOSE);
+    }
+}
